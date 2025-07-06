83e5cc0033fc6a56e238e686507c659253e739e0
Devesh Jangid <deveshjangid524@gmail.com>
2025-07-06 16:56:42 +0530

    create slice for customers in redux

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -5,7 +5,7 @@ import {MdTableBar } from 'react-icons/md';
 import {CiCircleMore } from 'react-icons/ci';
 import {BiSolidDish} from 'react-icons/bi';
 
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, useLocation} from 'react-router-dom';
 import { useState } from 'react';
 import Modal from './Modal';
 
@@ -13,6 +13,7 @@ import Modal from './Modal';
 
 const Footer = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const [isModalOpen,setIsModalOpen] = useState(false);
   const [guestCount,setGuestCount] = useState(0);
   const increament = () => {
@@ -30,18 +31,20 @@ const decreament = () => {
   const closeModal = () =>{
     setIsModalOpen(false);
   }
+
+  const isActive = (path) => location.pathname === path;
   return (
     <div className='fixed bottom-0 lef-0 right-0 bg-[#262626] p-2 h-16 w-full flex justify-around'>
-      <button onClick={()=> navigate('/')} className='flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px]
-      '><FaHome className='inline mr-2' size={20}/> <p>Home</p></button>
+      <button onClick={()=> navigate('/')} className={`cursor-pointer hover:bg-[#3a3a3a] flex items-center justify-center font-bold ${isActive('/') ? 'text-[#f5f5f5] bg-[#343434]':'text-[#ababab]'}  w-[200px] rounded-[20px]
+      `}><FaHome className='inline mr-2' size={20}/> <p>Home</p></button>
 
-      <button onClick={()=> navigate('/orders')} className='flex items-center justify-center text-[#ababab] w-[200px]'><MdOutlineReorder className='inline mr-2' size={20}/><p>Orders</p></button>
+      <button onClick={()=> navigate('/orders')}  className={`hover:bg-[#3a3a3a] cursor-pointer flex items-center justify-center font-bold ${isActive('/orders') ? 'text-[#f5f5f5] bg-[#343434]':'text-[#ababab]'}  w-[200px] rounded-[20px]`}><MdOutlineReorder className='inline mr-2' size={20}/><p>Orders</p></button>
 
-      <button onClick={()=> navigate('/tables')} className='flex items-center justify-center text-[#ababab] w-[200px]'><MdTableBar className='inline mr-2' size={20}/>Tables</button>
+      <button onClick={()=> navigate('/tables')}  className={`hover:bg-[#3a3a3a] cursor-pointer  flex items-center justify-center font-bold ${isActive('/tables') ? 'text-[#f5f5f5] bg-[#343434]':'text-[#ababab]'}  w-[200px] rounded-[20px]`}><MdTableBar className='inline mr-2' size={20}/>Tables</button>
 
-      <button className='flex items-center justify-center text-[#ababab] w-[200px]'><CiCircleMore className='inline mr-2' size={20}/>More</button>
+      <button className='hover:bg-[#3a3a3a] flex items-center justify-center text-[#ababab] cursor-pointer w-[200px]'><CiCircleMore className='inline mr-2' size={20}/>More</button>
 
-        <button onClick={ openModal} className='absolute bottom-6 bg-[#F6B100] text-[#f5f5f5] items-center rounded-full p-3'><BiSolidDish size={30}/></button>
+        <button disabled={isActive('/tables') || isActive('/menu')} onClick={ openModal} className='absolute bottom-6 bg-[#F6B100] text-[#f5f5f5] items-center rounded-full p-3 cursor-pointer'><BiSolidDish size={30}/></button>
 
         <Modal isOpen={isModalOpen} onClose={closeModal} title="Create Order">
 
--- frontend/src/components/tables/TableCard.jsx
+++ frontend/src/components/tables/TableCard.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { getRandomBG } from '../../utils'
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const TableCard = ({name,status,initials,seats}) => {
-  const navigate=useNavigate();
-  const handleClick=()=>{
-    if(status==='Booked') return;
+const TableCard = ({ name, status, initials, seats }) => {
+  const navigate = useNavigate();
+
+  
+  const bgColor = useMemo(() => getRandomBG(), []);
+
+  const handleClick = () => {
+    if (status === 'Booked') return;
     navigate('/menu');
   }
+
   return (
-    <div onClick={handleClick} className='w-[300px] bg-[#262626] p-4 rounded-lg  cursor-pointer hover:bg-[#2c2c2c]'>
+    <div onClick={handleClick} className='w-[300px] bg-[#262626] p-4 rounded-lg cursor-pointer hover:bg-[#2c2c2c]'>
       <div className='flex items-center justify-between px-1'>
-        <h1 className='text-[#f5f5f5] text-xl font-semibold'>{name} </h1>
-        <p className={`${status === 'Booked'? "text-green-600 bg-[#2e4a40]": "bg-[#664a04] text-white"} px-2 py-1 rounded-lg`}>{status}</p>
+        <h1 className='text-[#f5f5f5] text-xl font-semibold'>{name}</h1>
+        <p className={`${status === 'Booked' ? "text-green-600 bg-[#2e4a40]" : "bg-[#664a04] text-white"} px-2 py-1 rounded-lg`}>{status}</p>
       </div>
       <div className='flex justify-center items-center mt-5 mb-7'>
-        <h1 className={`${getRandomBG()} text-white rounded-full p-5 text-xl`}>{initials}</h1>
+        <h1 className={`${bgColor} text-white rounded-full p-5 text-xl`}>{initials}</h1>
       </div>
+      <p className='text-[#ababab] text-xs'>Seats: <span className='text-[#f5f5f5]'>{seats}</span></p>
     </div>
   )
 }
