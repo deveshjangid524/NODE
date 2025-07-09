14ee10cf8e52c0f36b60705648a60bf68df938d9
Devesh Jangid <deveshjangid524@gmail.com>
2025-07-09 15:43:33 +0530

    use dispatch to send login data to store

--- app/frontend/src/components/auth/Login.jsx
+++ app/frontend/src/components/auth/Login.jsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react'
 import {useMutation} from '@tanstack/react-query'
 import {login} from '../../https/index'
 import { enqueueSnackbar } from 'notistack'
+import { useDispatch } from 'react-redux'
+import { setUser } from '../../redux/slices/userSlice'
+import { useNavigate } from 'react-router-dom'
 const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -22,6 +27,9 @@ const Login = () => {
     onSuccess: (res) => {
         const {data} = res;
         console.log(data);
+        const { _id , name,email,phone,role} = data.data;
+        dispatch(setUser({_id , name,email,phone,role}))
+        navigate('/');
     },
     onError: (error) => {
         const {response} = error;
